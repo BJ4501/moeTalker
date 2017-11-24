d49ffc7e4815752bc9be7c3639f7758644104499
BJ4501 <neko.bj4501@gmail.com>
2017-11-24 14:59:52 +0800

    Restful接口测试

--- src/main/java/com/bj/web/moetalker/push/Application.java
+++ src/main/java/com/bj/web/moetalker/push/Application.java
@@ -1,4 +1,27 @@
 package com.bj.web.moetalker.push;
 
-public class Application {
+
+import com.bj.web.moetalker.push.service.AccountService;
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.util.logging.Logger;
+
+public class Application extends ResourceConfig {
+    public Application(){
+        //注册逻辑处理
+        //两种写法
+        //packages("com.bj.web.moetalker.push.service");
+        packages(AccountService.class.getPackage().getName());
+
+        //注册Json转换器
+        register(JacksonJsonProvider.class);
+        //注册日志打印输出
+        register(Logger.class);
+
+
+    }
+
+
+
 }
---
+++ src/main/java/com/bj/web/moetalker/push/service/AccountService.java
@@ -0,0 +1,32 @@
+package com.bj.web.moetalker.push.service;
+
+import com.bj.web.moetalker.push.bean.User;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+//真实访问路径---> localhost/api/account
+@Path("/account")
+public class AccountService {
+
+    @GET
+    @Path("/login")
+    public String get(){
+        return "you --> /login";
+    }
+
+    @POST
+    @Path("/login")
+    //指定请求与返回的响应体为JSON
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User post(){
+
+        User u = new User();
+        u.setName("Aaa");
+        u.setSex(2);
+        return u;
+    }
+
+
+}
