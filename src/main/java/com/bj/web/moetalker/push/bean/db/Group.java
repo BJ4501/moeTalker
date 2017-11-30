c8a8368e85c7dc0eeb5e07d5a2c3bfa11ad1c86c
BJ4501 <neko.bj4501@gmail.com>
2017-11-30 16:36:16 +0800

    Group 与User，Message之间的关联关系设置

--- src/main/java/com/bj/web/moetalker/push/bean/db/Group.java
+++ src/main/java/com/bj/web/moetalker/push/bean/db/Group.java
@@ -43,6 +43,18 @@ public class Group {
     @Column(nullable = false)
     private LocalDateTime updateAt = LocalDateTime.now();
 
+    //群的创建者
+    //optional：可选为false，必须有一个创建者
+    //fetch：加载方式FetchType.EAGER，急加载
+    //意味着加载群的信息的时候就必须加载owner的信息
+    //cascade:连级级别ALL，如果owner进行更新，那么所有的更改(更新，删除等)都将进行关系更新
+    @ManyToOne(optional = false,fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinColumn(name = "ownerId")
+    private User owner;
+    @Column(nullable = false,updatable = false,insertable = false)
+    private String ownerId;
+
+
     public String getId() {
         return id;
     }
@@ -89,5 +101,21 @@ public class Group {
 
     public void setUpdateAt(LocalDateTime updateAt) {
         this.updateAt = updateAt;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
     }
 }
--- src/main/java/com/bj/web/moetalker/push/bean/db/Message.java
+++ src/main/java/com/bj/web/moetalker/push/bean/db/Message.java
@@ -49,7 +49,7 @@ public class Message {
     // 不能自己修改，不能自己插入
     // 这个字段仅仅只是为了对应sender的数据库字段senderId
     // 不允许手动的更新或插入
-    @Column(updatable = false,insertable = false)
+    @Column(nullable = false,updatable = false,insertable = false)
     private String senderId;
 
     //接收者，可为空，多个消息对应一个接收者
@@ -58,6 +58,13 @@ public class Message {
     private User receiver;
     @Column(updatable = false,insertable = false)
     private String receiverId;
+
+    //一个群可以接收多个消息
+    @ManyToOne
+    @JoinColumn(name = "groupId")
+    private Group group;
+    @Column(updatable = false,insertable = false)
+    private String groupId;
 
 
     // 定义为创建时间戳，在创建时就已经写入
@@ -149,5 +156,21 @@ public class Message {
 
     public void setReceiverId(String receiverId) {
         this.receiverId = receiverId;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+    public String getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
     }
 }
